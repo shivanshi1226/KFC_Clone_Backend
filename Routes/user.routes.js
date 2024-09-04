fe4e7aa03b4df8e0ad01c9ac283a3c8a0a014a8e
shivanshi1226 <shivanshishailja26@gmail.com>
2024-09-04 23:29:59 +0530

    changes in package

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const Model = require('../Model/user.model')
 
 const userRoutes = express.Router();
